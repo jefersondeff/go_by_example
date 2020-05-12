52fa0b1a04d2d97c8995f894a478c4276f2852ea
jefersondeff <jefersonactf@gmail.com>
2020-05-11 23:56:29 -0300

    add example of variables

---
+++ variables/main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var declares 1 or more variables.
+	var a = "initial"
+	fmt.Println(a)
+
+	// You can declare multiple variables at once.
+	var b, c int = 1, 2
+	fmt.Println(b, c)
+
+	// Go will infer the type of initialized variables.
+	var d = true
+	fmt.Println(d)
+
+	// variable declared whiout a corresponding initialization are zero-valued.
+	// for example, the zero value for an int is 0
+	var e int
+	fmt.Println(e)
+
+	// the := syntax is shorthand for declaring and initializing a variable,
+	// e.g for var f string = "apple"
+	f := "apple"
+	fmt.Println(f)
+
+}
